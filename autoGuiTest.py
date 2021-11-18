045d43f55564986818a7e3d651a278e544d0efc7
zzttqu <1161085395@qq.com>
2021-11-18 19:18:24 +0800

    修改了文件名称
    实现了更快速的骚扰

--- autoGuiTest.py
+++ autoGuiTest.py
@@ -2,6 +2,7 @@ import pyautogui
 import time
 from matchPicture import imgAutoClick
 import pyperclip
+
 # 获取当前屏幕分辨率
 screenWidth, screenHeight = pyautogui.size()
 X, Y = imgAutoClick('QQ.png')
@@ -10,7 +11,7 @@ print(X, Y)
 pyautogui.moveTo(X, Y, duration=0.3, tween=pyautogui.linear)
 pyautogui.click(x=None, y=None, interval=0, button='left')
 time.sleep(1)
-X, Y = imgAutoClick('fhy.png')
+X, Y = imgAutoClick('ww.png')
 print(X, Y)
 pyautogui.moveTo(X, Y, duration=0.3, tween=pyautogui.linear)
 pyautogui.click(x=None, y=None, interval=0, button='left')
@@ -21,14 +22,17 @@ pyautogui.doubleClick(x=X, y=Y, interval=0.0, button='left', duration=0.0, tween
 # X, Y = pyautogui.locateCenterOnScreen('fhy.png')
 # 这一行是双击
 time.sleep(0.5)
-for _ in range(10):
-    pyautogui.typewrite(message='dalao', interval=0)
-    pyautogui.press('space')
-    pyautogui.typewrite(message='tql', interval=0)
+for _ in range(30):
+    # 光打英文不太行，还是直接复制粘贴比较好
+    pyperclip.copy('大佬tql')
+    # pyperclip.paste()  # 这样是不行的，没法往特定的位置填写，只能获取到本地
+    pyautogui.hotkey('ctrl', 'v')
+    # pyautogui.typewrite(message='dalao', interval=0)
+    # pyautogui.press('space')
+    # pyautogui.typewrite(message='tql', interval=0)
+    # pyautogui.press('enter')
+    # time.sleep(0.05)
     pyautogui.press('enter')
-    #  time.sleep(0.05)
-    pyautogui.press('enter')
-
 
 # while True:
 #     currentMouseX, currentMouseY = pyautogui.position()
